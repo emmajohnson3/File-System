8873fbcb2d074936b892d52b51f23b0b78acdf3e
emma <emmajohnson@uvic.ca>
2019-04-11 09:29:53 -0700

    adding dir

--- disk/RW.c
+++ disk/RW.c
@@ -252,9 +252,9 @@ int createDirectory(FILE* disk, char* data) {
                         printf("reading dir\n");
                         char* buffer = malloc(sizeof(char) * BLOCK_SIZE);
                         readBlock(disk, 2, buffer);
-                        printf("%s", buffer[0]);
-                        printf("%s", buffer[1]);
-                        printf("%s", buffer[2]);
+                        printf("%c", buffer[0]);
+                        printf("%c", buffer[1]);
+                        printf("%c", buffer[2]);
               }   
               break;
 
