e8b5cb4452146360005b69ddb195d2522cf5c229
emma <emmajohnson@uvic.ca>
2019-04-11 18:29:33 -0700

    adding tests

--- disk/script.sh
+++ disk/script.sh
@@ -10,5 +10,10 @@ hexdump -C vdisk
 gcc test2.c -o test2 RW.c -std=c99
 ./test2
 
-echo "This is what the vdisk looks like: "
+echo "This is what the vdisk looks like now: "
 hexdump -C vdisk
+
+gcc test3.c -o test3 RW.c -std=c99
+./test3
+
+echo "End of script"
---
+++ disk/test3.c
@@ -0,0 +1,49 @@
+#include "RW.h"
+
+int main(int argc, char* argv[]) {
+    char* buffer =  malloc(sizeof(char) * 512 * 2);
+    FILE* disk = fopen("vdisk", "w+b");
+    printf("This fil tests deleting directiories\n\n");
+   /* 
+    printf("This fil tests directories\n\n");
+    printf("We know the disk is persistant because the old files are still here:\n");
+    ReadDirectory(disk, "root");
+    printf("\n");
+
+    printf("Creating 'par' directory in root folder:\n");
+    createDirectory(disk, "par");
+    printf("Creating 'sub' directory in 'par' folder:\n");
+    createDirectory(disk, "par/sub");
+
+    printf("We can list the content of these directories:\n");
+    ReadDirectory(disk, "root");
+    printf("\n");
+    ReadDirectory(disk, "par");
+    printf("\n");
+    ReadDirectory(disk, "par/sub");
+    printf("\n\n");
+
+    printf("files can also be added to new directories:\n");
+    printf("Adding file 'I like dogs' to dir 'par':\n");
+    int dogs = createFile(disk, "par","I like dogs");
+    printf("Adding file 'I like mice' to dir 'par/sub':\n");
+    int mice = createFile(disk, "par","I like mice");
+
+    printf("\nThe files are now in the dirs and can be read:\n");
+    ReadDirectory(disk, "par");
+    ReadDirectory(disk, "par/sub");
+    printf("\n");
+    printf("Reading the dog file: %d\n", dogs);
+    buffer = readFile(disk,dogs);
+    printf("%s\n\n", buffer);
+
+    printf("Reading the mice file: %d\n", mice);
+    buffer = readFile(disk,mice);
+    printf("%s\n\n", buffer);
+
+    free(buffer);
+    printf("Finished the second test file\n\n");
+*/
+    fclose(disk);
+    return 0;
+}
