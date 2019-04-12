6a95851b46bddf8115634be103b1f42a01edc359
emma <emmajohnson@uvic.ca>
2019-04-11 18:23:12 -0700

    adding tests

--- disk/script.sh
+++ disk/script.sh
@@ -4,8 +4,11 @@ echo "Bash script running"
 gcc test1.c -o test1 RW.c -std=c99
 ./test1
 
+echo "This is what the vdisk looks like: "
+hexdump -C vdisk
+
 gcc test2.c -o test2 RW.c -std=c99
 ./test2
 
 echo "This is what the vdisk looks like: "
-hexdump -C vidisk
+hexdump -C vdisk
