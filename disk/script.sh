59afa8915c1ad1698e38b7cf7f3ca1bb75055e1d
emma <emmajohnson@uvic.ca>
2019-04-11 18:18:47 -0700

    adding tests

--- disk/script.sh
+++ disk/script.sh
@@ -6,3 +6,6 @@ gcc test1.c -o test1 RW.c -std=c99
 
 gcc test2.c -o test2 RW.c -std=c99
 ./test2
+
+echo "This is what the vdisk looks like: "
+hexdump -C vidisk
--- disk/test2.c
+++ disk/test2.c
@@ -3,7 +3,6 @@
 int main(int argc, char* argv[]) {
     char* buffer =  malloc(sizeof(char) * 512 * 2);
     FILE* disk = fopen("vdisk", "w+b");
-    CreateDisk(disk);
     
     printf("This fil tests directories\n\n");
     printf("We know the disk is persistant because the old files are still here:\n");
@@ -21,7 +20,25 @@ int main(int argc, char* argv[]) {
     ReadDirectory(disk, "par");
     printf("\n");
     ReadDirectory(disk, "par/sub");
+    printf("\n\n");
+
+    printf("files can also be added to new directories:\n");
+    printf("Adding file 'I like dogs' to dir 'par':\n");
+    int dogs = createFile(disk, "par","I like dogs");
+    printf("Adding file 'I like mice' to dir 'par/sub':\n");
+    int mice = createFile(disk, "par","I like mice");
+
+    printf("\nThe files are now in the dirs and can be read:\n");
+    ReadDirectory(disk, "par");
+    ReadDirectory(disk, "par/sub");
     printf("\n");
+    printf("Reading the dog file: %d\n", dogs);
+    buffer = readFile(disk,dogs);
+    printf("%s\n\n", buffer);
+
+    printf("Reading the mice file: %d\n", mice);
+    buffer = readFile(disk,mice);
+    printf("%s\n\n", buffer);
 
     free(buffer);
     printf("Finished the second test file\n\n");
