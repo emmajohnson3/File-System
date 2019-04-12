2815646189f5be99204b2885aafe3b5130457441
emma <emmajohnson@uvic.ca>
2019-04-11 19:30:19 -0700

    adding tests

--- disk/script.sh
+++ disk/script.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 echo "Bash script running"
-gcc test1.c -o test1 RW.c -std=c99
 ./test1
 
 echo "This is what the vdisk looks like: "
 hexdump -C vdisk
-
-gcc test2.c -o test2 RW.c -std=c99
 ./test2
 
 echo "This is what the vdisk looks like now: "
 hexdump -C vdisk
 
-gcc test3.c -o test3 RW.c -std=c99
 ./test3
 echo "End of test script"
