bd40cad274d5920949e9b8d0b47668cfebd24c20
emma <emmajohnson@uvic.ca>
2019-04-11 17:15:33 -0700

    adding tests

--- disk/test1.c
+++ disk/test1.c
@@ -5,11 +5,11 @@ int main(int argc, char* argv[]) {
     FILE* disk = fopen("vdisk", "w+b");
     CreateDisk(disk);
 
-    printf("This file tests reading and writing files\n");
+    printf("This file tests reading and writing files\n\n");
     
     printf("Writing large file (multiple blocks) of 'y2apffIWMWnotk8uP0k2KuR0MkDpswtqMEJkGP4TcA.... ect'\n");
     int file = createFile(disk, "root","y2apffIWMWnotk8uP0k2KuR0MkDpswtqMEJkGP4TcA1KgAc7d3AfAB78IaRTtNMtobRYefjXl0XzmKcRnvyU9Y006Z1raS0W0sZj8tgHbK4UXfTSpIpcFW3HelloaaaaaaacC5Pobfr9scWZeCgleJamIx4upRyl7Dx10pcKyXAe0N7BjZki6Ve1giPhtUwvItZDPUxs8NAbEoxxosl87aXBT8zCjv7xX6SwDb9s6jXI0fhUQ1o1qfSjGftUmi5mQ4CHcFlBTfhNBOOe3PIPKvaJ5Kwex3U5V25vbhB7ayEtJoHqDBtD70GOqpUfCpN8QazApuJd0301D5Rl0B6NXd54YBlDlI4tgwvqx6aosqZ99WjGaTutg5Ew8IpRn9lMTg9B53AeEcDn9mgQzA4R7rcGO8X189OA5BQH1W0ZWOJ0vGcsXcSXm5GLcAh3IG4P5h8WNYOpmcz14Ezmb2Fnf67RpTBYnI0tz1Mh2hOt04TPswPUDHJpv3z2tu47CNm6UJ5HrRG6nUr1GkaFPt02IOpaepwFqJ4bnVZLxT3fzA0oKaXKvCeMv02dpGMVL6bCFi0wtF4Lo9lHC5A9NqcawYBooop"); 
-    printf("Function returned id of %d\n",file);
+    printf("Function returned id of %d\n\n",file);
 
     printf("Writing small file of 'hello person there!!!!1'\n");
     int file2 = createFile(disk, "root","hello person there!!!!1");
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]) {
 
     printf("Reading the first file: %d\n", file);
     buffer = readFile(disk,file);
-    printf("%s\n", buffer);
+    printf("%s\n\n", buffer);
 
     printf("Reading the second file: %d\n", file2);
     buffer = readFile(disk,file2);
@@ -27,7 +27,7 @@ int main(int argc, char* argv[]) {
     ReadDirectory(disk, "root");
 
     free(buffer);
-    printf("done\n");
+    printf("Finished the first test file\n\n");
 
     fclose(disk);
     return 0;
